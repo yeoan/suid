1c3d31bef1299f8cdea86f46d9244ef0a62b2bd6
蔣曜安 <yeoan11317@gmail.com>
2019-02-26 19:53:47 +0800

    add mySecondFile and modify myFirstFile

--- myFirstFile.js
+++ myFirstFile.js
@@ -1,7 +1,15 @@
 let a = "410312011" //區域變數
 
+let b = "UN" //系級
+
 function showID(){
   console.log(a)//system.out.print()
 }
 
 showID()
+
+function showDep(){
+  console.log(b)
+}
+
+showDep()
---
+++ mySecondFile.js
@@ -0,0 +1,7 @@
+let name = "蔣曜安"
+
+function showname(){
+  console.log(name)
+}
+
+showname()
